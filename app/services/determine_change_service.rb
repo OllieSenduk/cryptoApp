446818509b91925292adca7f434846216c160557
Lars Bohm <larsbohm@gmail.com>
2017-12-17 15:05:24 +0100

    fix determine change services to include current_value_in_euro

--- app/services/determine_change_service.rb
+++ app/services/determine_change_service.rb
@@ -5,25 +5,22 @@ class DetermineChangeService
   end
 
   def call
+    current_value_of_crypto_in_euro = @coin_session.amount_in_crypto * current_price_in_euro
+    current_price_in_euro = SingleCoinDataService.new(coin: @coin_session.coin).call[0]["price_eur"].to_f
     if coin_in_best?
-
-      current_price_in_euro = SingleCoinDataService.new(coin: @coin_session.coin).call[0]["price_eur"].to_f
-      current_value_of_crypto_in_euro = @coin_session.amount_in_crypto * current_price_in_euro
-
-
       if current_value_of_crypto_in_euro >= @coin_session.last_known_value
         # POTENTIALLY KEEP
         @coin_session.last_known_value = current_value_of_crypto_in_euro
         @coin_session.save
       elsif current_value_of_crypto_in_euro < @coin_session.last_known_value
         # SELL NOW
-        SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session).call
+        SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session, current_value_of_crypto_in_euro: current_value_of_crypto_in_euro).call
       else
         # I dunno
       end
 
     else # SELL NOW
-      SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session).call
+      SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session, current_value_of_crypto_in_euro: current_value_of_crypto_in_euro).call
     end
   end
 
