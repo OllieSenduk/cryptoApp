45fe965049d6688dd56c0b07450f3132a9f967cf
Lars Bohm <larsbohm@gmail.com>
2017-12-17 14:59:59 +0100

    update index on home and coins on trade_process show

--- app/controllers/trade_processes_controller.rb
+++ app/controllers/trade_processes_controller.rb
@@ -3,5 +3,6 @@ class TradeProcessesController < ApplicationController
 
   def show
     @trade_process = TradeProcess.find(params[:id])
+    @coins = Coin.order("coin_sessions_count ASC").paginate(:page => params[:page], :per_page => 15)
   end
 end
