dad9ec85fe913722229b86cd905938519a9b9da0
Ollie <ollie_senduk@hotmail.com>
2017-12-18 22:44:49 +0100

    changed best bet as just one strategy

--- app/models/trade_process.rb
+++ app/models/trade_process.rb
@@ -2,4 +2,22 @@ class TradeProcess < ApplicationRecord
   has_many :coin_sessions
   has_many :coins, through: :coin_sessions
   has_one :rest_amount
+
+  after_create :decide_strategy
+  # Add a strategy model, name
+  # belongs_to trade_process
+  # after_create :decide_strategy -> Add it to the tradeprocess
+  # Go into a Service dependent on the strategy
+
+  private
+
+  def decide_strategy
+    if TradeProcess.last.strategy != "best_bet" || TradeProcess.last.nil?
+      self.update(strategy: "best_bet")
+    else TradeProcess.last.strategy == "best_bet"
+      self.update(strategy: "best_bet")
+    end
+  end
+
+
 end
