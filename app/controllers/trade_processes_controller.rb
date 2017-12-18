e6f4358f2b4f414543538e2e2aad4e73f489d51a
Lars Bohm <larsbohm@gmail.com>
2017-12-18 14:58:45 +0100

    added basic styling for logging cards and improved logging mechanic

--- app/controllers/trade_processes_controller.rb
+++ app/controllers/trade_processes_controller.rb
@@ -4,6 +4,6 @@ class TradeProcessesController < ApplicationController
   def show
     @trade_process = TradeProcess.find(params[:id])
     @coins = Coin.order("coin_sessions_count ASC").paginate(:page => params[:page], :per_page => 15)
-    @coin_sessions = @trade_process.coin_sessions.paginate(:page => params[:page], :per_page => 8)
+    @coin_sessions = @trade_process.coin_sessions.order('updated_at DESC').paginate(:page => params[:page], :per_page => 4)
   end
 end
--- app/services/determine_change_service.rb
+++ app/services/determine_change_service.rb
@@ -2,16 +2,19 @@ class DetermineChangeService
   def initialize(attributes)
     @best_bet_outcome = attributes[:best_bet_outcome]
     @coin_session = attributes[:coin_session]
+    @session_log = @coin_session.session_log
   end
 
   def call
     current_price_in_euro = SingleCoinDataService.new(coin: @coin_session.coin).call[0]["price_eur"].to_f
     @current_value_of_crypto_in_euro = @coin_session.amount_in_crypto * current_price_in_euro
     if coin_in_best?
-      @coin_session.session_log.change_log << "\nCoin in best - DetermineChangeService"
+      @session_log.change_log << "\nCoin in best - DetermineChangeService | #{Time.now.in_time_zone("Amsterdam").strftime("%H:%M on %A, %e %B %Y")}"
+      @session_log.save
       check_value
     else # SELL NOW
-      @coin_session.session_log.change_log << "\nNot in best - DetermineChangeService"
+      @session_log.change_log << "\nNot in best - DetermineChangeService | #{Time.now.in_time_zone("Amsterdam").strftime("%H:%M on %A, %e %B %Y")}"
+      @session_log.save
       SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session, current_value_of_crypto_in_euro: @current_value_of_crypto_in_euro).call
     end
   end
@@ -24,16 +27,23 @@ class DetermineChangeService
 
   def check_value
     if @current_value_of_crypto_in_euro >= @coin_session.last_known_value
-      @coin_session.session_log.change_log << "\nValue went up - DetermineChangeService (#{@current_value_of_crypto_in_euro} => #{@coin_session.last_known_value})"
+      if @current_value_of_crypto_in_euro == @coin_session.last_known_value
+        @session_log.change_log << "\nValue unchanged - DetermineChangeService (#{@coin_session.last_known_value.round(3)} => #{@current_value_of_crypto_in_euro.round(3)})"
+      else
+        @session_log.change_log << "\nValue unchanged - DetermineChangeService (#{@coin_session.last_known_value.round(3)} => #{@current_value_of_crypto_in_euro.round(3)})"
+      end
+      @session_log.save
       # POTENTIALLY KEEP
       @coin_session.last_known_value = @current_value_of_crypto_in_euro
       @coin_session.save
     elsif @current_value_of_crypto_in_euro < @coin_session.last_known_value
       # SELL NOW
-      @coin_session.session_log.change_log << "\nValue dropped - DetermineChangeService (#{@current_value_of_crypto_in_euro} => #{@coin_session.last_known_value})"
+      @session_log.change_log << "\nValue dropped - DetermineChangeService (#{@coin_session.last_known_value.round(3)} => #{@current_value_of_crypto_in_euro.round(3)})"
+      @session_log.save
       SellNowService.new(best_bet_outcome: @best_bet_outcome, coin_session: @coin_session, current_value_of_crypto_in_euro: @current_value_of_crypto_in_euro).call
     else
-      @coin_session.session_log.change_log << "\nI dunno - DetermineChangeService"
+      @session_log.change_log << "\nI dunno - DetermineChangeService"
+      @session_log.save
       # I dunno
     end
   end
