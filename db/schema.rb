ede4fbafdd63060f1b7615fea777826aeb254380
Ollie <ollie_senduk@hotmail.com>
2017-12-19 20:27:01 +0100

    added payload

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218201137) do
-
+ActiveRecord::Schema.define(version: 20171219192018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "backtests", force: :cascade do |t|
+    t.jsonb "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "coin_sessions", force: :cascade do |t|
     t.bigint "trade_process_id"
