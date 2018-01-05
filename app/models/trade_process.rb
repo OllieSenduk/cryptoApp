727464a7d88c6a9a6a90b2012e191189be1cd4de
Lars Bohm <larsbohm@gmail.com>
2018-01-05 13:46:28 +0100

    change strategy creation to service

--- app/models/trade_process.rb
+++ app/models/trade_process.rb
@@ -3,17 +3,4 @@ class TradeProcess < ApplicationRecord
   has_many :coins, through: :coin_sessions
   has_one :rest_amount
 
-  after_create :decide_strategy
-
-  private
-
-  def decide_strategy
-    if TradeProcess.last.strategy != "best_bet" || TradeProcess.last.nil?
-      self.update(strategy: "best_bet")
-    else TradeProcess.last.strategy == "best_bet"
-      self.update(strategy: "best_bet")
-    end
-  end
-
-
 end
